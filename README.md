765f934baad12fc53cbe015d345d626e6d4e5577
Oleg Subachev <solsw@posolsoft.com>
2022-03-19 16:43:21 +0500

    v2 released

--- README.md
+++ README.md
@@ -6,4 +6,3 @@ future
 Package **future** contains a generic implementation of [explicit](https://en.wikipedia.org/wiki/Futures_and_promises#Implicit_vs._explicit) [future](https://en.wikipedia.org/wiki/Futures_and_promises).
 
 Since **future** uses generics it requires at least Go 1.18.
-Use [go1.18rc1](https://go.dev/dl/#go1.18rc1) or [gotip](https://pkg.go.dev/golang.org/dl/gotip) to experiment with **future**.
--- future.go
+++ future.go
@@ -23,19 +23,23 @@ type Future[Result any] struct {
 	once sync.Once
 	wg   sync.WaitGroup
 
-	hasResult bool
-	res       Result
-	err       error
+	depleted bool
+	res      Result
+	err      error
 }
 
 // New creates a new Future.
 //
 // 'ctx' - initial context (is passed to 'promise' as is if 'timeout' is not provided).
+//
 // 'promise' produces the Future's result. 'promise' is called exactly once.
+// 'promise' must respect 'ctx' cancelling to not leak the goroutine.
+// If 'promise' does not complete before 'timeout' elapses, 'promise' is discarded and ErrPromiseTimeout is returned.
+//
 // 'timeout' - timeout to wait for 'promise' to complete (pass zero if timeout is not needed).
 // If 'timeout' is provided, new context (that is canceled when 'timeout' elapses)
-// is derived from 'ctx' and passed to 'promise'. 'promise' must respect context cancelling to not leak goroutine.
-// If 'promise' does not complete before 'timeout' elapses, 'promise' is discarded and ErrPromiseTimeout is returned.
+// is derived from 'ctx' and passed to 'promise'.
+//
 // If 'lazy', 'promise' will be called synchronously by the first Result() call, otherwise - asynchronously immediately.
 func New[Result any](ctx context.Context, promise func(context.Context) (Result, error), timeout time.Duration, lazy bool) *Future[Result] {
 	f := Future[Result]{
@@ -53,7 +57,7 @@ func New[Result any](ctx context.Context, promise func(context.Context) (Result,
 
 func (f *Future[Result]) getResult() {
 	defer func() {
-		f.hasResult = true
+		f.depleted = true
 		f.wg.Done()
 	}()
 	if f.timeout <= 0 {
@@ -75,7 +79,7 @@ func (f *Future[Result]) getResult() {
 		go func() {
 			defer close(ch)
 			res, err := f.promise(ctx)
-			// promise may be already canceled or timed out here
+			// promise may be already canceled or deadlined here
 			if f.err != nil {
 				return
 			}
@@ -100,5 +104,5 @@ func (f *Future[Result]) Result() (Result, error) {
 
 // Depleted reports whether the Future already has a result or/and an error.
 func (f *Future[Result]) Depleted() bool {
-	return f.hasResult
+	return f.depleted
 }
